a1b5c4bb519321e1cbaa1ff9906eee3fe30c2a7f
Willian Mohr <willianbmohr@gmail.com>
2025-05-03 13:12:34 -0300

    first commit

---
+++ src/main/java/com/wohr/movie_rent_clean_arch/MovieRentCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.wohr.movie_rent_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wohr/movie_rent_clean_arch/MovieRentCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wohr.movie_rent_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
